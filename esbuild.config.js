c7c9ce5b755b4eb59e7ff5f0a95a4e41411405ae
pynickle <2330458484@qq.com>
2025-12-09 23:18:54 +0800

    chore: back to cjs build and downgrade uuid

--- esbuild.config.js
+++ esbuild.config.js
@@ -3,8 +3,8 @@
 await build({
     entryPoints: ['src/index.ts'],
     bundle: true,
-    outfile: 'lib/index.js',
-    format: 'esm',
+    outfile: 'lib/index.cjs',
+    format: 'cjs',
     platform: 'node',
     external: [
         'koishi',
