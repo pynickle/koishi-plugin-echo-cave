e4ef49e64654fd7159fd0a277968e03c2a3f56ef
pynickle <2330458484@qq.com>
2026-04-19 15:28:54 +0800

    fix: fix esbuild

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,15 +1,17 @@
-﻿import { build } from 'esbuild'
+﻿import {build} from 'esbuild'
 
 await build({
-    entryPoints: ['src/index.ts'],
-    bundle: true,
-    outfile: 'lib/index.cjs',
-    format: 'cjs',
-    platform: 'node',
-    external: [
-        'koishi',
-        '@pynickle/koishi-plugin-adapter-onebot',
-        'axios',
-        'uuid'
-    ]
+  entryPoints: ['src/index.ts'],
+  bundle: true,
+  outfile: 'lib/index.cjs',
+  format: 'cjs',
+  platform: 'node',
+  external: [
+    'koishi',
+    '@pynickle/koishi-plugin-adapter-onebot',
+    'axios',
+    'uuid',
+    '@aws-sdk/s3-request-presigner',
+    '@aws-sdk/client-s3'
+  ]
 })
